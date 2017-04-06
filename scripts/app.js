7225db1ac24d922876f4c4e112153c3ea3de1b52
sejinpark0209 <sejinpark0209@gmail.com>
2017-04-05 20:19:38 -0700

    not completed yet

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,37 @@
-$(document).on("ready", function(){
 
+
+
+$(document).on("ready", function(){
+	//Sample Response, Trending, use for loop to print out 25 top images on the page
+
+	$.ajax({
+		method: "GET",
+		url: "http://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC",
+		dataType: "Json",
+		success: onSuccess,
+		data: $('form').serialize()
+	})
+
+	function onSuccess(response) {
+		for(var i = 0; i < 25; i++) {
+			$('.gif-gallery').append('response.data[i].type.url')
+		}
+		// for(var i = 0; i < .data[i].type.image.fixed_height.url; i++) {
+		// 	console.log(data[i].type.image.fixed_height.url);
+	}
+
+
+	$.ajax({
+		method: "GET",
+		url: "http://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC",
+		dataType: "Json",
+		success: findImage,
+		data: $('form').serialize()
+	})
+	function findImage(response) {
+		for(var i = 0; i < 9; i++) {
+			$('.gif-gallery').attr("src", response.data[i].type.url)
+		}
+
+	}
 });
